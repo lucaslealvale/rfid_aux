0de6ff6d71e72c68333dcd34a89d9d0e02b1eb91
lucaslealvale <lucaslealvale01@gmail.com>
2021-09-30 09:46:44 -0300

    new features

--- testes/teste.c
+++ testes/teste.c
@@ -7,56 +7,112 @@
 #define BASE_REG_SET 0
 #define BASE_REG_TARI 1
 #define BASE_REG_FIFO 2
+#define BASE_REG_STATUS 3
 #define BASE_ID 7
 #define PACKET_STD_SIZE 12
 #define data_package_size 26
 #define data_mask_size 6
+#define eop 0b00000000000000000000000000000000
 
-int tari_test = 0b111110100;
+int tari_test         = 0b111110100;
+int command_test      = 0b11111111111111111111111111111111;
+int command_test_size = 32;
 
-int command_ack = 0b111111111111111111111111111111;
 
-void send_package(int pacote, int size)
-{
-    int remain_bits_to_send = size;
-    int remain_package = pacote;
-    printf("Pacote= %d , remain_package = %d\n", pacote, remain_package);
-    int last_package_size = size % data_package_size;
+// int check_fifo_full(){
+//     //return IORD_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE,BASE_REG_STATUS<<3) & 1;}
+//     return 0;}
+// void send_package(int package){
+//     //IOWR_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_FIFO << 2, package);
+//     printf("Resume:data_to_fifo = %04X\n", package);
+//     printf("---------------------------------------------------------- \n");
+//     }
+// void send_end_of_package(){
+//     //IOWR_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_FIFO << 2, eop);}
+//     printf("Resume:data_to_fifo = %04X\n", eop);
+//     printf("---------------------------------------------------------- \n");
+// //commands = [ 0b00001110001,
+// //             0b11111111111,
+// //             0b00000000011,
+// //             0b01010100101,
+// //             0b11001100110]
+// void select_package(int commands[]){
+// 	int commands_bits = sizeof(commands);
+// 	int lengh_commands = commands_bits/sizeof(commands[0]);
 
-    while (remain_bits_to_send > 0)
-    {
-        printf("remain_bits_to_send = %d \n", remain_bits_to_send);
+// 	for(int command = 0;command < lengh_commands;command++ ){
+// 		if(sizeof(commands[command]) ){
+// 		mount_package(commands[command], command_test_size);
+// 	}
+// } // TODO
 
-        if (remain_bits_to_send > data_package_size)
-        {
-            printf("Package Bigger Than fifo capacity------------------------ \n");
-            printf("remain_pack = %04X \n", remain_package);
-            int data_to_fifo = remain_package & 0x1A;
-            printf("data_to_fifo = %04X \n", data_to_fifo);
-            remain_package = remain_package >> 0x1A;
-            int32_t to_fifo = data_to_fifo << 6 | 0x1A;
-            remain_bits_to_send = remain_bits_to_send - 0x1A;
-            printf("Resume:data_to_fifo = %04X\nmask_package = %04X\nremain_package= %04X\nto_fifo = %04X\n", data_to_fifo, last_package_size, remain_package, to_fifo);
-            printf("--------------------------------------------------------- \n");
-        }
-        else
-        {
-            printf("Package Smaller Than fifo capacity------------------------ \n");
-            printf("remain_pack = %04X \n", remain_package);
-            int data_to_fifo = remain_package;
-            printf("data_to_fifo = %04X \n", data_to_fifo);
-            remain_package = remain_package >> last_package_size;
-            int32_t to_fifo = data_to_fifo << 6 | last_package_size;
-            remain_bits_to_send = remain_bits_to_send - last_package_size;
-            printf("Resume:data_to_fifo = %04X\nmask_package = %04X\nremain_package= %04X\nto_fifo = %04X\n", data_to_fifo, last_package_size, remain_package, to_fifo);
-            printf("---------------------------------------------------------- \n");
-        }
-        printf("\n");
-    }
-}
+// void mount_package(int command, int size)
+// {
+//     int remain_bits_to_send = size;
+//     int remain_package = command;
+//     int last_package_size = size % data_package_size;
+
+//     while (remain_bits_to_send > 0)
+//     {
+//         if (remain_bits_to_send > data_package_size)
+//         {
+//             int data_to_fifo = remain_package & 0x1A;
+//             remain_package = remain_package >> 0x1A;
+//             int32_t to_fifo = data_to_fifo << 6 | 0x1A;
+
+//             while(check_fifo_full()){}
+
+//             send_package(to_fifo);
+
+//             remain_bits_to_send = remain_bits_to_send - 0x1A;
+//         }
+//         else
+//         {
+//             int data_to_fifo = remain_package;
+//             remain_package = remain_package >> last_package_size;
+//             int32_t to_fifo = data_to_fifo << 6 | last_package_size;
+
+//             while(check_fifo_full()){}
+
+//             send_package(to_fifo);
+//             remain_bits_to_send = remain_bits_to_send - last_package_size;
+//         }
+//     }
+//     send_end_of_package();
+// }
 
 int main()
 {
-    send_package(command_ack, 30);
+    //IOWR_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_TARI << 2, tari_test);
+    //IOWR_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_SET << 2, MASK_EN);
+    // ack_init(&command_ack, 1234);
+    // ack_build(&command_ack);
+    //mount_package(command_ack.result_data, command_ack.size);
+    //mount_package(command_test, command_test_size);
+    int commands[4];
+    commands[0] = 0b11111111111111111111111111111111; //32
+    commands[1] = 0b00000000; //8
+    commands[2] = 0b1111111111111111; //16
+    commands[3] = 0b11111111111111111111111111111111; //32
+    int commands_bits = sizeof(commands);
+	int lengh_commands = commands_bits/sizeof(commands[0]);
+    int c0 = sizeof(commands[0]);
+	int c1 = commands[1];
+    int c2 = commands[2];
+    int c3 = sizeof(commands[3])/sizeof(int);
+
+    printf("commands_bits = %d\nlengh_commands = %d\n", commands_bits, lengh_commands);
+    printf("c0 = %04X\nc1 = %04X\nc2 = %04X\nc3 = %04X\n", c0,c1,c2,c3);
+    unsigned bits, var = (c2 < 0) ? -c2 : c2;
+    for(bits = 0; var != 0; ++bits) var >>= 1;
+    printf("bits: %d, var: %d", bits, var);
+    // for(int command = 0;command < lengh_commands;command++ ){
+	// 	if(sizeof(commands[command]) )
+	// 	mount_package(commands[command], command_test_size);
+	// }
     return 0;
 }
+
+
+// printf("Resume:data_to_fifo = %04X\nmask_package = %04X\nremain_package= %04X\nto_fifo = %04X\n", data_to_fifo, last_package_size, remain_package, to_fifo);
+// printf("---------------------------------------------------------- \n");
--- testes/testeInNios.c
+++ testes/testeInNios.c
@@ -14,15 +14,32 @@
 #define PACKET_STD_SIZE 12
 #define data_package_size 26
 #define data_mask_size 6
-#define eop 0b000000000000000000000000000000
+#define eop 0b00000000000000000000000000000000
+
 int tari_test = 0b111110100;
+int command_test = 0b11111111111111111111111111111111;
+int command_test_size = 32;
 ack command_ack;
 
-int check_fifo_full(){return IORD_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE,BASE_REG_STATUS<<3) & 1;}
+int check_fifo_full() { return IORD_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_STATUS << 3) & 1; }
 
-void send_package(int package){IOWR_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_FIFO << 2, package);}
+void send_package(int package) { IOWR_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_FIFO << 2, package); }
+void send_end_of_package() { IOWR_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_FIFO << 2, eop); }
 
-//int select_package(int commands[], int n ){} // TODO
+void select_package(int commands[])
+{
+
+    int lengh_commands = sizeof(commands) / sizeof(commands[0]);
+
+    for (int command = 0; command < lengh_commands; command++)
+    {
+        // https://stackoverflow.com/questions/29388711/c-how-to-get-length-of-bits-of-a-variable
+        unsigned command_size, var = (commands[command] < 0) ? -commands[command] : commands[command];
+        for (command_size = 0; var != 0; ++command_size)
+            var >>= 1;
+        mount_package(commands[command], command_size);
+    }
+}
 
 void mount_package(int command, int size)
 {
@@ -37,11 +54,11 @@ void mount_package(int command, int size)
             int data_to_fifo = remain_package & 0x1A;
             remain_package = remain_package >> 0x1A;
             int32_t to_fifo = data_to_fifo << 6 | 0x1A;
-            
-            while(check_fifo_full()){}
-            
+
+            while (check_fifo_full()){}
+
             send_package(to_fifo);
-            
+
             remain_bits_to_send = remain_bits_to_send - 0x1A;
         }
         else
@@ -49,14 +66,14 @@ void mount_package(int command, int size)
             int data_to_fifo = remain_package;
             remain_package = remain_package >> last_package_size;
             int32_t to_fifo = data_to_fifo << 6 | last_package_size;
-            
-            while(check_fifo_full()){}
+
+            while (check_fifo_full()){}
 
             send_package(to_fifo);
             remain_bits_to_send = remain_bits_to_send - last_package_size;
         }
     }
-    send_package(eop);
+    send_end_of_package();
 }
 
 int main()
@@ -65,7 +82,13 @@ int main()
     IOWR_32DIRECT(NIOS_RFID_PERIPHERAL_0_BASE, BASE_REG_SET << 2, MASK_EN);
     ack_init(&command_ack, 1234);
     ack_build(&command_ack);
-    mount_package(command_ack.result_data, command_ack.size);
-    //mount_package(pacotao, pacotaosize);
+    //mount_package(command_ack.result_data, command_ack.size);
+    //mount_package(command_test, command_test_size);
+    int commands[4];
+    commands[0] = 0b11111111111111111111111111111111; //32
+    commands[1] = 0b00000000; //8
+    commands[2] = 0b1111111111111111; //16
+    commands[3] = 0b010101010101010101010101010101; //30
+    select_package(commands);
     return 0;
 }
